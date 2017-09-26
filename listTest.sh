3eb3a756885649f61dc0b7a7570a5fb46080d204
sanfo3855 <sanfelicimatteo@gmail.com>
2017-09-26 17:21:57 +0200

    a

--- listTest.sh
+++ listTest.sh
@@ -1,35 +1,29 @@
-for (( i = 0; i < 220; i++ )); do
-    echo ""
-    echo "***************"
-    echo "***** " $i " *****"
-    echo "***************"
-    ./JoUnit.sh https://github.com/sanfo3855/JoEC &&
-    ./JoUnit.sh https://github.com/sanfo3855/test1 &&
+./JoUnit.sh https://github.com/sanfo3855/JoEC &&
+./JoUnit.sh https://github.com/sanfo3855/test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1 &&
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1 &&
 
-    ./JoUnit.sh ../JoEC  &&
-    ./JoUnit.sh ../test1
-done
+./JoUnit.sh ../JoEC  &&
+./JoUnit.sh ../test1
